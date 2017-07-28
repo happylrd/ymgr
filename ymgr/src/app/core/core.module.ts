2b5e39422b63dc3c497fc96726f4913a23edfd16
happylrd <happylrd17@gmail.com>
2017-07-28 16:47:06 +0800

    Add basic header and footer
    with material

--- ymgr/src/app/core/core.module.ts
+++ ymgr/src/app/core/core.module.ts
@@ -1,11 +1,25 @@
 import {NgModule, SkipSelf, Optional} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MdToolbarModule} from '@angular/material'
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MdToolbarModule
   ],
-  declarations: []
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
